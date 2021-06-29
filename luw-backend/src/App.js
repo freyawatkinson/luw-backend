bce4d958a6f577f3c586684c532d66ab63d441a5
Freya Watkinson <67429855+freyawatkinson@users.noreply.github.com>
2021-06-30 10:16:43 +1200

    updated css for login page

--- luw-backend/src/App.js
+++ luw-backend/src/App.js
@@ -3,43 +3,48 @@ import React, { useState } from 'react';
 import './App.css';
 
 import Axios from 'axios';
+import LoginPage from './Components/LoginPage';
+import Layout from './Components/Layout';
 
 function App() {
 
-  const [usernameReg, setUsernameReg] = useState("");
-  const [passwordReg, setPasswordReg] = useState("");
+  // const [usernameReg, setUsernameReg] = useState("");
+  // const [passwordReg, setPasswordReg] = useState("");
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  // const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState("");
 
-  const [loginStatus, setLoginStatus] = useState("");
+  // const [loginStatus, setLoginStatus] = useState("");
 
-  const register = () => {
-    Axios.post('http://localhost3002/register', {
-      username: usernameReg,
-      password: passwordReg,
-    }).then((response) => {
-      console.log(response);
-    }) ;
-  }
+  // const register = () => {
+  //   Axios.post('http://localhost3002/register', {
+  //     username: usernameReg,
+  //     password: passwordReg,
+  //   }).then((response) => {
+  //     console.log(response);
+  //   }) ;
+  // }
 
-  const login = () => {
-    Axios.post('http://localhost3002/login', {
-      username: username,
-      password: password,
+  // const login = () => {
+  //   Axios.post('http://localhost3002/login', {
+  //     username: username,
+  //     password: password,
 
-    }).then((response) => {
-      if (response.data.message) {
-        setLoginStatus(response.data.message)
-      } else {
-        setLoginStatus(response.data[0].username)
-      }
-    }) ;
-  }
+  //   }).then((response) => {
+  //     if (response.data.message) {
+  //       setLoginStatus(response.data.message)
+  //     } else {
+  //       setLoginStatus(response.data[0].username)
+  //     }
+  //   }) ;
+  // }
 
   return (
     <div className="App">
-      <div className="registration">
+      <Layout/>
+      <LoginPage/>
+      
+      {/* <div className="registration">
         <h1>Registration</h1>
         <label>Username</label>
         <input type="text" onChange={(e) => {
@@ -54,7 +59,7 @@ function App() {
         <input type="text" placeholder="Enter Username"></input>
         <input type="text" placeholder="Enter Password"></input>
         <button>Login</button>
-      </div>
+      </div> */}
     </div>
   );
 }
---
+++ luw-backend/src/Components/Layout.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import './Layout.css';
+import headerLogo from './headerLogo.png';
+import { Link } from 'react-router-dom';
+
+
+const Layout = ({ children }) => {
+    return (
+        <>
+            <nav className="nav">
+                <div className="headerBox">
+                
+
+                    {/* <Link to="/"> */}
+                        <img className="headerLogo" alt="logo" src={headerLogo} />
+                    {/* </Link> */}
+
+                    <div className="hehe">
+                        {/* <Link to="/"> */}
+                            <p className="homeLink">Home</p>
+                        {/* </Link> */}
+
+                        {/* <Link to="/CourseDetails"> */}
+                            <p className="courseDetLink">Course detail</p>
+                        {/* </Link> */}
+{/* 
+                        <Link to="/bookingPage"> */}
+                            <p className="bookingLink">Booking Consultation</p>
+                        {/* </Link> */}
+
+                        {/* <Link to="/aboutUs"> */}
+                            <p className="aboutUsLink">About us</p>
+                        {/* </Link> */}
+
+                        {/* <Link to="/Login"> */}
+                            <p className="accLogo">logo</p>
+                        {/* </Link> */}
+                    </div>
+                </div>
+            </nav>
+
+
+            <main>
+                {children}
+            </main>
+
+            <footer>
+                <div className="footerBox">
+                    <p className="name">LevelUp Works</p>
+                    <p className="copyRight">2021 - All right reserved</p>
+                    <p className="icons">icons will go here</p>
+
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Layout;
